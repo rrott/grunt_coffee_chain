f6b7db1940a2aea87aade53fa8997246a3a3df5e
Rrott <roman.rott@ecommerce.com>
2013-11-25 21:24:25 +0200

    removed useless for now methods

--- tasks/coffee_chain.coffee
+++ tasks/coffee_chain.coffee
@@ -1,10 +1,9 @@
 #TODO: rewrite these coffee scripts to use coffee_chain in head of this plugin
 "use strict"
 module.exports = (grunt) ->
-  fileFinder = require('./lib/file_finder').init(grunt);
+  #fileFinder = require('./lib/file_finder').init(grunt);
 
   grunt.registerMultiTask "coffeeChain", "grunt's task for concatenating CoffeeScript files that have 'require' directive in correct order", ->
-    debug = require('debug')('coffeeChain:coffeeChain');
     options =  @options(
       keyword:    '#= require'
       dirKeyword: '#= require_tree'
@@ -13,4 +12,4 @@ module.exports = (grunt) ->
 
     )
 
-    fileFinder.searchFiles(@files, options)
+    #fileFinder.searchFiles(@files, options)
--- tasks/coffee_chain.js
+++ tasks/coffee_chain.js
@@ -1,18 +1,14 @@
 (function() {
   "use strict";
   module.exports = function(grunt) {
-    var fileFinder;
-    fileFinder = require('./lib/file_finder').init(grunt);
     return grunt.registerMultiTask("coffeeChain", "grunt's task for concatenating CoffeeScript files that have 'require' directive in correct order", function() {
-      var debug, options;
-      debug = require('debug')('coffeeChain:coffeeChain');
-      options = this.options({
+      var options;
+      return options = this.options({
         keyword: '#= require',
         dirKeyword: '#= require_tree',
         extension: '.coffee',
         separator: grunt.util.linefeed
       });
-      return fileFinder.searchFiles(this.files, options);
     });
   };
 
