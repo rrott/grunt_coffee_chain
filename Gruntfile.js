867949b20bf881a939484daa78de2d6b89ede7fe
Rrott <roman.rott@ecommerce.com>
2014-01-20 18:00:08 +0200

    added missed gruntfile.js

---
+++ Gruntfile.js
@@ -0,0 +1,45 @@
+(function() {
+  "use strict";
+  module.exports = function(grunt) {
+    grunt.initConfig({
+      clean: {
+        tests: ["tmp"]
+      },
+      nodeunit: {
+        tests: ["test/*_test.js"]
+      },
+      coffeeChain: {
+        compile: {
+          src: 'tasks/coffee_chain.coffee',
+          dest: 'tasks/coffee_chain.js'
+        },
+        test: {
+          src: 'test/coffee_chain_test.coffee',
+          dest: 'test/coffee_chain_test.js'
+        },
+        compileGruntfile: {
+          src: 'Gruntfile.coffee',
+          dest: 'Gruntfile.js'
+        },
+        default_options: {
+          src: "test/fixtures/default_options.coffee",
+          dest: "tmp/default_options.js"
+        },
+        custom_options: {
+          options: {
+            minify: true
+          },
+          src: "test/fixtures/default_options.coffee",
+          dest: "tmp/custom_options.js"
+        }
+      }
+    });
+    grunt.loadTasks("tasks");
+    grunt.loadNpmTasks("grunt-contrib-clean");
+    grunt.loadNpmTasks("grunt-contrib-nodeunit");
+    grunt.loadNpmTasks("grunt-coffee-chain");
+    grunt.registerTask("test", ["clean", "coffeeChain", "nodeunit"]);
+    return grunt.registerTask("default", ["coffeeChain", "test"]);
+  };
+
+}).call(this);
