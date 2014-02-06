c70f874963669869e0f38eadeba5ac1b9863d2cc
Rrott <roman.rott@ecommerce.com>
2014-02-06 03:13:18 +0200

    slight bugfixes in compiler

--- src/lib/messages.coffee
+++ src/lib/messages.coffee
@@ -1,6 +1,7 @@
 class root.Messages
   constructor: ->
     @messages =
+      description:  "grunt's task for concatenating CoffeeScript files that have 'require' directive in correct order"
       missed_param: 'You should provide "src" and "dest" params in the Gruntfile'
 
   text: (text) ->
--- tasks/coffee_chain.js
+++ tasks/coffee_chain.js
@@ -2,6 +2,7 @@
   root.Messages = (function() {
     function Messages() {
       this.messages = {
+        description: "grunt's task for concatenating CoffeeScript files that have 'require' directive in correct order",
         missed_param: 'You should provide "src" and "dest" params in the Gruntfile'
       };
     }
